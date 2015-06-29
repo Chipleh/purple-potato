e4fbaad2cef405a9dd207309be312c379f6e43c7
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-06-29 12:23:24 -0700

    - complete file structure check in and inclusion of the main.css into the leidos templates

--- drupal/sites/all/themes/leidos/template.php
+++ drupal/sites/all/themes/leidos/template.php
@@ -21,6 +21,9 @@ function leidos_preprocess_html(&$variables) {
     $variables['classes_array'][] = 'footer-columns';
   }
 
+  // Leidos Stylesheet
+  drupal_add_css(path_to_theme() . '/css/main.css', array('group' => CSS_THEME);
+
   // Add conditional stylesheets for IE
   drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
   drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
