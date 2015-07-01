f638c1292500f02d89570a58b01736f776556390
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-07-01 15:05:43 -0700

    - desktop search box complete

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function() {
 
 	var searchBox = jQuery('li.last span.nolink');
+	var cancelButton = jQuery('#search_leidos_website button[type=cancel]');
 	var searchForm = jQuery('div.block-leidos-custom-search');
 
 	// search click
-	jQuery('li.last span.nolink').on('click', function() {
+	jQuery(searchBox).on('click', function() {
 		jQuery(searchBox).toggleClass('active');
 		jQuery(searchForm).toggleClass('active');
 	});
 
-	// search click
-	// jQuery('li.last span.nolink').on('click', function() {
-	// 	jQuery(searchBox).toggleClass('active');
-	// 	jQuery(searchBox).toggleClass('active');
-	// });
+	//search click
+	jQuery(cancelButton).on('click', function(e) {
+		jQuery(searchBox).toggleClass('active');
+		jQuery(searchForm).toggleClass('active');
+		e.preventDefault();
+	});
 
 });
