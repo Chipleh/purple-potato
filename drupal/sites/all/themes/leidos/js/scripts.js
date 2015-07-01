ebbb388bcb96fd6990159b80d05f38fb3678b96b
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-07-01 12:12:33 -0700

    - search slider [unstyled, but working]

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -1,3 +1,18 @@
-$('li.last span.nolink').on('click', function() {
-	console.log("working"); 
+jQuery(document).ready(function() {
+
+	var searchBox = jQuery('li.last span.nolink');
+	var searchForm = jQuery('div.block-leidos-custom-search');
+
+	// search click
+	jQuery('li.last span.nolink').on('click', function() {
+		jQuery(searchBox).toggleClass('active');
+		jQuery(searchForm).toggleClass('active');
+	});
+
+	// search click
+	// jQuery('li.last span.nolink').on('click', function() {
+	// 	jQuery(searchBox).toggleClass('active');
+	// 	jQuery(searchBox).toggleClass('active');
+	// });
+
 });
--- drupal/sites/all/themes/leidos/js/scripts/search-box.js
+++ drupal/sites/all/themes/leidos/js/scripts/search-box.js
@@ -1,3 +1,10 @@
-$('li.last span.nolink').on('click', function() {
-	console.log("working"); 
+jQuery(document).ready(function() {
+
+	// search click
+	jQuery('li.last span.nolink').on('click', function() {
+		var searchBox = jQuery('li.last span.nolink');
+		var searchForm = jQuery('div.block-leidos-custom-search');
+		jQuery(searchBox).toggleClass('active');
+	});
+
 });
