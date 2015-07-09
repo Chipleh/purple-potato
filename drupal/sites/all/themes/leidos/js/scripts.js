ab1b984176985b4447b753d80cd83b91abff076a
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-07-09 16:42:59 -0700

    - side nav [tablet: 50% complete]

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -1,3 +1,35 @@
+//	Top Nav JS
+//	Rhino Hooton
+//	6/29/15
+jQuery(document).ready(function() {
+
+	var menuAnchor = jQuery(".parent-menu > li a");
+	var menuSubMenu = jQuery(".parent-menu > li .no-children");
+	var menuSideMobile = jQuery(".left-navigation-menu .parent-menu li.expanded span");
+	var menuSideParentMobile = jQuery(".left-navigation-menu .parent-menu");
+
+	// desktop
+	jQuery(menuAnchor).hover(
+		function() {
+			jQuery(this).parent("li").toggleClass("hover");
+		}
+	);
+
+	jQuery("menuSubMenu").hover(
+		function() {
+			jQuery(this).parent("li").toggleClass("hover");
+		}
+	);
+
+	// tablet/mobile
+	jQuery(menuSideMobile).click(
+		function() {
+			jQuery(this).parent("li").toggleClass("mobile-active");
+			jQuery(menuSideParentMobile).toggleClass("parent-shift");
+		}
+	);
+});
+
 //	Search Box JS
 //	Rhino Hooton
 //	6/29/15
