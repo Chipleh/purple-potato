edb813bf6d3485b2582ac243320ec7f80721af28
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-07-09 11:28:57 -0700

    - desktop top nav & side nav check in.

--- drupal/sites/all/themes/leidos/js/scripts/top-nav.js
+++ drupal/sites/all/themes/leidos/js/scripts/top-nav.js
@@ -3,16 +3,19 @@
 //	6/29/15
 jQuery(document).ready(function() {
 
-	jQuery(".expanded a").hover(
+	var menuAnchor = jQuery(".parent-menu > li a");
+	var menuSubMenu = jQuery(".parent-menu > li .no-children");
+
+	// desktop
+	jQuery(menuAnchor).hover(
 		function() {
-			jQuery(this).parent(".expanded").toggleClass("hover");
-		}
-	);
-	jQuery(".no-children").hover(
-		function() {
-			jQuery(this).parent(".expanded").toggleClass("hover");
+			jQuery(this).parent("li").toggleClass("hover");
 		}
 	);
 
+	jQuery("menuSubMenu").hover(
+		function() {
+			jQuery(this).parent("li").toggleClass("hover");
+		}
+	);
 });
-
