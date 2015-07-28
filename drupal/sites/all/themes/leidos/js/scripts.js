432d63b489af775c31cc10457ecc42f2a3a05a4e
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-07-28 15:20:41 -0700

    - new capabilities module [raw]

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -1,3 +1,21 @@
+//	Capabilities Module
+//	Rhino Hooton
+//	6/29/15
+jQuery(document).ready(function() {
+	jQuery('.sliding-box').hover(function() {
+		jQuery(this).toggleClass('hover');
+	});
+
+	jQuery('.sliding-box').on('click', function () {
+		if (jQuery(this).hasClass('open')) {
+			jQuery(this).removeClass('open');
+			return false;
+		}
+		jQuery('.open').removeClass('open');
+		jQuery(this).toggleClass('open');
+	});
+});
+
 //	Top Nav JS
 //	Rhino Hooton
 //	6/29/15
