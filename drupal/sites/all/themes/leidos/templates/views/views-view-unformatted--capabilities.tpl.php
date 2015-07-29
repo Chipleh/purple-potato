f104a2c85a439b555ffed61b446b5606c69ecea8
Zak Huber <zak.huber@mirumagency.com>
2015-07-29 10:33:23 -0700

    Updated draggableviews and link for capabilities view

--- drupal/sites/all/themes/leidos/templates/views/views-view-unformatted--capabilities.tpl.php
+++ drupal/sites/all/themes/leidos/templates/views/views-view-unformatted--capabilities.tpl.php
@@ -1,6 +1,9 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
+<?php if (user_access('administer content')): ?>
+  <h3><?php print l(t('[Change sort order]'), 'admin/sorting/project-types'); ?></h3>
+<?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <?php print $row; ?>
 <?php endforeach; ?>
