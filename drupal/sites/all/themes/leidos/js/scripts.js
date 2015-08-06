e23231dadcf68c7e53b04b8f40bc3c07ae5d577c
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-08-06 15:51:04 -0700

    QA check in 1

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -121,6 +121,16 @@ jQuery(document).ready(function() {
 	setLeftNav();
 
 });
+//	Projects JS
+//	Rhino Hooton
+//	6/29/15
+jQuery(document).ready(function() {
+
+	jQuery('.pane-projects .views-row, .pane-projects-full-view-projects-pane .views-row, .pane-projects-full-view-projects-pane-mobile .views-row').hover(function() {
+		jQuery(this).toggleClass("hover");
+	});
+
+});
 //	Search Box JS
 //	Rhino Hooton
 //	6/29/15
