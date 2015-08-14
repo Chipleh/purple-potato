f2d8cacee3793c80ad883af3cf433f0c9ecc1044
Zak Huber <zak.huber@mirumagency.com>
2015-08-14 10:31:36 -0700

    Revert "refs #99305 - Left menu navigation tree consolidation"
    This reverts commit 5edc92dffa26d7613f291e3e5169c32528257f95.

--- drupal/sites/all/themes/leidos/templates/fields/field--field-menu.tpl.php
+++
@@ -1,5 +0,0 @@
-<span class="additional-control"><?php print t('Back'); ?></span>
-<span class="additional-info"></span>
-<ul class="menu clearfix menu-container">
-  <?php print $left_navigation; ?>
-</ul>
