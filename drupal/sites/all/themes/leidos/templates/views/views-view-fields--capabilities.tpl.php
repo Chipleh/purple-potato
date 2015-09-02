55729370cc43dc4fe23448fea57773c6e18e53db
Zak Huber <zak.huber@mirumagency.com>
2015-09-01 17:50:02 -0700

    refs #99629 - Helper text for capabilities images, refs #99621 - Hide hover/previews

--- drupal/sites/all/themes/leidos/templates/views/views-view-fields--capabilities.tpl.php
+++ drupal/sites/all/themes/leidos/templates/views/views-view-fields--capabilities.tpl.php
@@ -4,7 +4,7 @@
       <h2><span><?php print $fields['name']->content; ?></span></h2>
     </div>
     <div class="image-box"><?php print $fields['field_taxonomy_image']->content; ?></div>
-    <div class="slide-box-content <?php print str_replace(' ', '-', strtolower($fields['field_hover_shape']->content)); ?>">
+    <div class="slide-box-content hide-<?php print $fields['field_hide_hover']->content; ?> <?php print str_replace(' ', '-', strtolower($fields['field_hover_shape']->content)); ?>">
       <p><?php print $fields['field_hover_text']->content; ?></p>
       <span class="slide-box-more">More</span>
       <div class="slide-box-content-bg" style="background-color: <?php print $fields['field_bottom_color']->content; ?>;"></div>
@@ -13,7 +13,7 @@
   <div class="click-box-content">
     <div class="click-box-image">
       <?php print $fields['field_preview_image']->content; ?>
-      <div class="click-box-image-caption-container">
+      <div class="click-box-image-caption-container hide-<?php print $fields['field_hide_preview_box']->content; ?>">
         <div class="click-box-image-caption"><?php print $fields['field_preview_image_hover_text']->content; ?></div>
         <div class="click-box-image-additional-caption"><?php print $fields['field_preview_image_hover_text']->content; ?></div>
       </div>
