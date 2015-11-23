c00dccddf674e91320db8e6ca0ed97602743ac8d
Zak Huber <zak.huber@mirumagency.com>
2015-11-23 09:19:22 -0800

    revert compiled scripts.js

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -1,31 +1,27 @@
 //	Capabilities Module
 //	Rhino Hooton
 //	6/29/15
-(function ($) {
-  Drupal.behaviors.leidosCapabilities = {
-    attach: function (context, settings) {
-			// add hover state on mouse over
-			jQuery('.sliding-box .inner').hover(function() {
-				jQuery(this).parent('.sliding-box').toggleClass('hover');
-			});
+jQuery(document).ready(function() {
+	// add hover state on mouse over
+	jQuery('.sliding-box .inner').hover(function() {
+		jQuery(this).parent('.sliding-box').toggleClass('hover');
+	});
 
-			// open slide box on content click
-			jQuery('.sliding-box .inner').on('click', function () {
-				if (jQuery(this).parent('.sliding-box').hasClass('open')) {
-					jQuery(this).parent('.sliding-box').removeClass('open');
-					return false;
-				}
-				jQuery('.open').removeClass('open');
-				jQuery(this).parent('.sliding-box').toggleClass('open');
-			});
-
-			// close slide box on close click
-			jQuery('.sliding-box .click-box-close').on('click', function () {
-				jQuery('.open').removeClass('open');
-			});
+	// open slide box on content click
+	jQuery('.sliding-box .inner').on('click', function () {
+		if (jQuery(this).parent('.sliding-box').hasClass('open')) {
+			jQuery(this).parent('.sliding-box').removeClass('open');
+			return false;
 		}
-	};	
-})(jQuery);
+		jQuery('.open').removeClass('open');
+		jQuery(this).parent('.sliding-box').toggleClass('open');
+	});
+
+	// close slide box on close click
+	jQuery('.sliding-box .click-box-close').on('click', function () {
+		jQuery('.open').removeClass('open');
+	});
+});
 
 //	Top Nav JS
 //	Rhino Hooton
