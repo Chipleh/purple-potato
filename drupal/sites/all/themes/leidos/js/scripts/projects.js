81f23ed2b5dcf1c3125acf34c1c8aa3d4ff17f09
Rhino Hooton <rhino.hooton@mirumagency.com>
2015-11-24 14:38:36 -0800

    - projects issues resolved

--- drupal/sites/all/themes/leidos/js/scripts/projects.js
+++ drupal/sites/all/themes/leidos/js/scripts/projects.js
@@ -4,9 +4,13 @@
 (function ($) {
   Drupal.behaviors.leidosProjects = {
     attach: function (context, settings) {
-    	jQuery('.pane-projects .views-row, .pane-projects-full-view-projects-pane .views-row, .pane-projects-full-view-projects-pane-mobile .views-row').hover(function() {
-    		jQuery(this).toggleClass("hover");
-    	});
+    	console.log("loaded");
+    	jQuery('.pane-projects .views-row, .pane-projects-full-view-projects-pane .views-row, .pane-projects-full-view-projects-pane-mobile .views-row').on('mouseenter', function() {
+    		console.log("hovering");
+    		jQuery(this).addClass("hover");
+    	}).on("mouseleave", function() {
+    		jQuery(this).removeClass("hover");
+		}); 
     }
   };
 })(jQuery);
