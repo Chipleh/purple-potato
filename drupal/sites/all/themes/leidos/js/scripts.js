dc47dc3a992d959b4c66bf87bd118563cba2bcae
Rhino Hooton <rhino.hooton@mirumagency.com>
2016-02-24 13:55:17 -0800

    - oops

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -26,6 +26,42 @@
 		}
 	};	
 })(jQuery);
+//  Toggle background images (desktop/mobile) based on web browser width.
+//  Marc S. Brooks
+//  2/24/16
+(function ($) {
+	Drupal.behaviors.flexibleTemplateBackground = {
+		attach: function(context, settings) {
+
+			// Define styles for defined data-* attributes.
+			function defineStyles() {
+				var width  = jQuery('body').width(),
+				select = null;
+
+				if (width > 960) {
+					select = 'data-background-desktop';
+				} else {
+					select = 'data-background-mobile';
+				}
+
+				var items = jQuery('[' + select + ']');
+
+				items.each(function() {
+					var item = $(this),
+					val  = item.attr(select);
+
+					item.parent().attr('style', val);
+				});
+			}
+
+			// Listen for resize events.
+			window.onresize = defineStyles;
+
+			// Init on load.
+			defineStyles();
+		}
+	};
+})(jQuery);
 //	Capabilities Module
 //	Rhino Hooton
 //	6/29/15
---
+++ drupal/sites/all/themes/leidos/js/scripts/flex-backgrounds.js
@@ -0,0 +1,36 @@
+//  Toggle background images (desktop/mobile) based on web browser width.
+//  Marc S. Brooks
+//  2/24/16
+(function ($) {
+	Drupal.behaviors.flexibleTemplateBackground = {
+		attach: function(context, settings) {
+
+			// Define styles for defined data-* attributes.
+			function defineStyles() {
+				var width  = jQuery('body').width(),
+				select = null;
+
+				if (width > 960) {
+					select = 'data-background-desktop';
+				} else {
+					select = 'data-background-mobile';
+				}
+
+				var items = jQuery('[' + select + ']');
+
+				items.each(function() {
+					var item = $(this),
+					val  = item.attr(select);
+
+					item.parent().attr('style', val);
+				});
+			}
+
+			// Listen for resize events.
+			window.onresize = defineStyles;
+
+			// Init on load.
+			defineStyles();
+		}
+	};
+})(jQuery);
