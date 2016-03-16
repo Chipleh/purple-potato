9e9039716ff0f556aa51a4fd81ffc08cfb9e90a1
Marc S. Brooks <marc.brooks@digitaria.com>
2016-03-16 00:12:27 -0700

    Added IFRAME height/width defaults

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -81,8 +81,16 @@
       $('video > source', context)
         .each(function() {
           if ($(this).attr('type') == 'video/youtube') {
-            var iframe = $('<iframe>');
-            iframe[0].src = 'https://www.youtube.com/embed/' + $(this)[0].src.replace(/^.*\?v=(\w+)$/, '$1');
+            var iframe = $('<iframe>')
+              .css({
+                height: '610px',
+                width:  '1000px'
+              });
+
+            if ($(this)[0].src) {
+              iframe[0].src = 'https://www.youtube.com/embed/' + $(this)[0].src.replace(/^.*\?v=(\w+)$/, '$1');
+            }
+
             $(this).parent().replaceWith(iframe);
           }
         });
