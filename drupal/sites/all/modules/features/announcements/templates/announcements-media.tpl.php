43193bac4952b3b2c80639070f663edecf0b7abc
Zak Huber <zak.huber@mirumagency.com>
2016-04-06 12:08:21 -0700

    Announcements video output

--- drupal/sites/all/modules/features/announcements/templates/announcements-media.tpl.php
+++ drupal/sites/all/modules/features/announcements/templates/announcements-media.tpl.php
@@ -1,14 +1,17 @@
-<div class="announcements-media <?php print $alignment; ?>">
-  <?php if (!empty($image)) { ?>
-  <div class="image"><?php print $image; ?></div>
-  <?php } ?>
-  <?php if (!empty($video)) { ?>
-  <div class="video"><?php print $video; ?></div>
-  <?php } ?>
-  <?php if (!empty($caption)) { ?>
-  <div class="caption"><?php print $caption; ?></div>
-  <?php } ?>
-  <?php if (!empty($icons)) { ?>
-  <div class="icons"><?php print $icons; ?></div>
-  <?php } ?>
+<div id="announcment-detail">
+  <div class="announcements-media <?php print $alignment; ?>">
+    <?php if (!empty($image)) { ?>
+    <div class="image"><img src="<?php print $image; ?>"></div>
+    <?php } ?>
+    <?php if (!empty($video)) { ?>
+    <div class="video"><?php print $video; ?></div>
+    <?php } ?>
+    <?php if (!empty($caption)) { ?>
+    <div class="caption"><?php print $caption; ?></div>
+    <?php } ?>
+    <?php if (!empty($icons)) { ?>
+    <div class="icons"><?php print $icons; ?></div>
+    <?php } ?>
+  </div>
+  <div class="body"><?php print $body; ?></div>
 </div>
