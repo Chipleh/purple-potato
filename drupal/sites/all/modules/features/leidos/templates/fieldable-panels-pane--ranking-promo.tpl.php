79fd13ca4cde5fd711d6e5c74d9df32539aee808
Zak Huber <zak.huber@mirumagency.com>
2016-04-27 15:53:10 -0700

    refs #104039 - Ranking promo panel tpl

---
+++ drupal/sites/all/modules/features/leidos/templates/fieldable-panels-pane--ranking-promo.tpl.php
@@ -0,0 +1,11 @@
+<?php
+  // Uncomment this to see all data that was entered in the CMS for this panel.
+  //dpm($output, 'output');
+?>
+<div <?php if (!empty($pane_id)) { print 'id="' . $pane_id . '"'; } ?> class="ranking-promo columns-<?php print $output['ranking_columns']; ?>">
+  <?php foreach ($output['ranking'] as $ranking) { ?>
+    <div class="ranking-circle" style="background-color: <?php print $ranking['field_title_color']; ?>;">#<?php print $ranking['field_ranking_number']; ?></div>
+    <div class="title"><?php print $ranking['field_title_item']; ?></div>
+    <div class="cta"><?php print $ranking['field_cta']; ?></div>
+  <?php } ?>
+</div>
