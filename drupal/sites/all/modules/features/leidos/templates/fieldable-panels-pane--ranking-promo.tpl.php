1e9f9e3fcb27c6dba08c33f96a93aeb15d74f85b
Zak Huber <zak.huber@mirumagency.com>
2016-05-16 11:13:27 -0700

    refs #104357 - Optional color circle on promo list

--- drupal/sites/all/modules/features/leidos/templates/fieldable-panels-pane--ranking-promo.tpl.php
+++ drupal/sites/all/modules/features/leidos/templates/fieldable-panels-pane--ranking-promo.tpl.php
@@ -3,9 +3,12 @@
   //dpm($output, 'output');
 ?>
 <div <?php if (!empty($pane_id)) { print 'id="' . $pane_id . '"'; } ?> class="ranking-promo columns-<?php print $output['ranking_columns']; ?>">
- 	<?php foreach ($output['ranking'] as $ranking) { ?>
+  <?php foreach ($output['ranking'] as $ranking) {
+    $ranking_class = isset($ranking['field_title_color']) ? 'ranking-circle' : 'ranking-simple';
+    $ranking_style = isset($ranking['field_title_color']) ? 'style="background-color: ' . $ranking['field_title_color'] . ';"' : '';
+    ?>
  		<div class="ranking-container">
-			<div class="ranking-circle" style="background-color: <?php print $ranking['field_title_color']; ?>;">#<?php print $ranking['field_ranking_number']; ?></div>
+			<div class="<?php print $ranking_class; ?>" <?php print $ranking_style; ?>>#<?php print $ranking['field_ranking_number']; ?></div>
 			<div class="ranking-details">
 				<div class="title"><?php print $ranking['field_title_item']; ?></div>
 				<div class="cta"><?php print $ranking['field_cta']; ?></div>
