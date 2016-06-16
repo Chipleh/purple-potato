ddf9299f6416dc2cb2ebb1bcd83eb537d7abd644
Monte Greene <monte.greene@digitaria.com>
2016-06-16 14:39:44 -0700

    [WIP] refs #104594: static About page initial check-in.

---
+++ drupal/sites/all/themes/leidos/static-pages/about.php
@@ -0,0 +1,253 @@
+<?php include("opening.html"); ?>
+
+
+<div class="staticContent">
+
+    <section class="marqueeImage">
+        <div class="marqueeImage__image"></div>
+        <div class="marqueeImage__overlay"></div>
+    </section>
+
+    <section class="hero">
+        <div class="hero__feature">
+            <heading>
+                <h2>We turn challenges into opportunities.</h2>
+            </heading>
+            <p>We bring a mix of innovative technology and sector expertise to customers in <a href="/natsec">national security</a>, <a href="/health">health</a>, and <a href="/infrastructure">infrastructure</a>. We look for solutions that not only transform businesses, but change the world.</p>
+            <a href="/capabilities" class="cta cta__primary">View Our Capabilities<span class="arrowRight"></span></a>
+        </div>
+        <div class="hero__topicList">
+            <a href="/sites/default/files/Leidos_Fact_Sheet.pdf" class="cta cta__primary">View Our Fact Sheet<span class="arrowRight"></span></a>
+            <ul>
+                <li>Our approach to every challenge is <strong>HOLISTIC & SYSTEMATIC</strong></li>
+                <li>We see solutions others can't with <strong>IMPACTFUL ANALYTICS</strong></li>
+                <li>We make real life better with a <strong>HUMAN POINT-OF-VIEW</strong></li>
+            </ul>
+        </div>
+    </section>
+
+    <nav class="fixedNav">
+        <ul>
+            <li>
+                <a href="#corporate-responsibility">Corporate Responsibility</a>
+            </li>
+            <li>
+                <a href="#history">History</a>
+            </li>
+            <li>
+                <a href="#commitment-to-quality">Commitment To Quality</a>
+            </li>
+            <li>
+                <a href="#rankings-and-awards">Rankings &amp; Awards</a>
+            </li>
+            <li>
+                <a href="#leadership">Leadership</a>
+            </li>
+            <li>
+                <a href="#leidos-companies">Leidos Companies</a>
+            </li>
+        </ul>
+    </nav>
+
+    <section class="imageTout imageTout--imgLeft" id="corporate-responsibility">
+        <img class="imageTout__image borderImage" src="https://www.leidos.com/sites/default/files/about-landing-responsibility_0.jpg" width="324" height="324" alt="">
+        <div class="imageTout__meta">
+            <heading>
+                <h2>Our People.<br>Our Communities.<br>Our Environment.</h2>
+                <h3>We do business with respect and responsibility for all three.</h3>
+            </heading>
+            <p>
+                At Leidos, we take responsibility for improving the quality of life of our employees, their families and the communities in which we live and work. Our commitment extends beyond the contributions we make to solve difficult problems through science and technology. <a href="/about/corporate-responsibility">More on Corporate Responsibility »</a>
+            </p>
+        </div>
+    </section>
+
+    <section class="history" id="history">
+        <ul class="history__list">
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-01-OurLegacy.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>Our History</h2>
+                    </heading>
+                    <p>More than 40 years ago a small team of visionary scientists set out to solve big problems…</p>
+                    <a href="">Learn More</a>
+                </div>
+            </li>
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-02-1970.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>1970</h2>
+                        <h3>Posted first profits; won a new relationship</h3>
+                    </heading>
+                    <p>Leidos posted a profit after its first year, much to its founder’s surprise, and established its first long-term client, the Defense Nuclear Agency.</p>
+                </div>
+            </li>
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-03-1987.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>1984</h2>
+                        <h3>Helped design America’s Cup winner</h3>
+                    </heading>
+                    <p>We supported the technical design of the winning yacht for the America’s Cup and began conceptual design for a geologic nuclear waste repository in Nevada.</p>
+                </div>
+            </li>
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-04-1997.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>1996</h2>
+                        <h3>Made a bold, strategic acquisition</h3>
+                    </heading>
+                    <p>The company’s entrepreneurial spirit and small-company flexibility kept us at the forefront of new technology. We acquired domain name registrar Network Solutions and became one of the largest providers of Internet and e-commerce services.</p>
+                </div>
+            </li>
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-05-2008.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>2008</h2>
+                        <h3>Built first-of-its-kind solar community</h3>
+                    </heading>
+                    <p>We played a major role in helping Okotoks, Alberta become the first solar community in North America. Our Canadian subsidiary developed a solar-thermal system to capture energy from the summer sun and store it to heat homes through the long, cold winter.</p>
+                </div>
+            </li>
+            <li class="history__event" style="background-image: url(https://www.leidos.com/sites/all/themes/leidos/images/about/timeline-06-2013.jpg)">
+                <div class="history__listInner">
+                    <heading>
+                        <h2>2013</h2>
+                        <h3>Formally split into Leidos and SAIC</h3>
+                    </heading>
+                    <p>The company officially split into Leidos, a $6 billion solutions-focused business, and SAIC, a $4 billion services business.</p>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+    <section class="imageTout imageTout--imgRight" id="commitment-to-quality">
+        <div class="imageTout__meta">
+            <heading>
+                <h2>A Commitment to Quality</h2>
+                <h3>We take pride in the quality of our solutions and services. That’s why our standards are high and our commitment to quality is unwavering.</h3>
+            </heading>
+            <p>To us, quality means two things: satisfying our customers by delivering what they need on time and in budget and continuously improving processes so that our work is done right. <a href="/about/quality">Learn More »</a>
+            </p>
+        </div>
+        <img class="imageTout__image borderImage" src="https://www.leidos.com/sites/default/files/about-landing-quality_0.jpg" width="324" height="324" alt="">
+    </section>
+
+    <section class="awards" id="rankings-and-awards">
+        <heading>
+            <h2>Rankings & Awards</h2>
+            <a href="/about/rankings-awards" class="subheadLink">View All »</a>
+        </heading>
+        <div class="awards__row">
+            <article class="awards__item awards__item--textRight">
+                <dl>
+                    <dt>#16</dt>
+                    <dd>TOP 100 GOVERNMENT CONTRACTORS</dd>
+                </dl>
+                <footer>
+                    by <a href="http://washingtontechnology.com/" target="_blank">Washington Technology</a>
+                </footer>
+            </article>
+            <article class="awards__item awards__item--textLeft">
+                <dl>
+                    <dt>The Forrester Wave&trade;</dt>
+                    <dd>Managed Security Services: North America, Q4 2014 Report</dd>
+                </dl>
+                <p></p>
+            </article>
+            <article class="awards__item awards__item--textRight">
+                <dl>
+                    <dt>#40</dt>
+                    <dd>TOP 500 DESIGN FIRMS</dd>
+                </dl>
+                <footer>
+                    by <a href="http://enr.construction.com/" target="_blank">Engineering News-Record</a>
+                </footer>
+            </article>
+        </div>
+        <div class="awards__row">
+            <article class="awards__item awards__item--textLeft">
+                <dl>
+                    <dt>2016</dt>
+                    <dd>"Best for Vets" Employer</dd>
+                </dl>
+                <footer>
+                    by <a href="http://bestforvets.militarytimes.com/best-employers-for-veterans/2016/" target="_blank">Military Times</a>
+                </footer>
+            </article>
+            <article class="awards__item awards__item--textRight">
+                <dl>
+                    <dt>#75</dt>
+                    <dd>500 GREENEST BIG COMPANIES IN AMERICA</dd>
+                </dl>
+                <footer>
+                    by <a href="http://www.thedailybeast.com/newsweek.html" target="_blank">Newsweek</a>
+                </footer>
+            </article>
+            <article class="awards__item awards__item--textLeft">
+                <dl>
+                    <dt>2015</dt>
+                    <dd>Nunn-Perry Award</dd>
+                </dl>
+                <footer>
+                    by <a href="http://www.defense.gov/" target="_blank">U.S. Department of Defense</a>
+                </footer>
+            </article>
+        </div>
+    </section>
+
+    <section class="leadership" id="leadership">
+        <heading>
+            <h2>Corporate Leadership</h2>
+            <a href="/about/org" class="subheadLink">View All »</a>
+            <p>Meet the executive team responsible for leading 19,000 forward thinking employees towards shared success.</p>
+        </heading>
+        <div class="leadership__content">
+            <img class="leadership__image" src="https://www.leidos.com/sites/default/files/Roger-A-Krone.jpg" width="320" height="320" alt="">
+            <div class="leadership__meta">
+                <heading>
+                    <h3><a href="/about/leadership/krone-bio">Roger A. Krone</a></h3>
+                    <h4>Chairman & CEO</h4>
+                    <p>Roger A. Krone is Chairman and Chief Executive Officer of Leidos. With more than $5 billion in annual revenue and 19,000 employees worldwide, Leidos is a recognized global leader in solving important problems in national security, health, and engineering. Before being named CEO in July 2014,...</p>
+                    <a href="/about/leadership/krone-bio">Learn More »</a>
+                </heading>
+            </div>
+        </div>
+    </section>
+
+    <section class="companies" id="leidos-companies">
+        <heading>
+            <h2>Leidos Companies</h2>
+            <a href="/about/companies" class="subheadLink">View All »</a>
+        </heading>
+        <ul>
+            <li>
+                <h3>Leidos Health</h3>
+                <p>Leidos Health provides a wide variety of clinical, business, and IT solutions for healthcare enterprises.</p>
+                <a href="http://www.leidoshealth.com/" target="_blank">Learn More »</a>
+            </li>
+            <li>
+                <h3>Leidos Canada</h3>
+                <p>From information, environmental, and engineering solutions to healthcare technologies — Leidos Canadian operations provide a wide range of services and products to government agencies and private businesses.</p>
+                <a href="https://www.leidos.com/canada" target="_blank">Learn More »</a>
+            </li>
+            <li>
+                <h3>Varec, Inc.</h3>
+                <p>Varec, a wholly owned subsidiary of Leidos, has been a leading innovator in the petroleum and chemical sectors for 80+ years.</p>
+                <a href="http://www.varec.com/" target="_blank">Learn More »</a>
+            </li>
+        </ul>
+    </section>
+
+</div>
+
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+
+<script>
+(function($){
+
+})(jQuery);
+</script>
+
+<?php include("closing.html"); ?>
