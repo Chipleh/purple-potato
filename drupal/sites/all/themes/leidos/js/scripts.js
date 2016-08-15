3aee3492ed5b8c6bcdc5cfc79b39e3bb6a67f115
Kenny Meleta <kenny.meleta@mirumagency.com>
2016-08-15 11:02:37 -0600

    Bug #105712 - uncommented relevant code

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -376,14 +376,14 @@
 //	Capabilities Module
 //	Rhino Hooton
 //	6/29/15
-// (function ($) {
-//   Drupal.behaviors.flexWorkaround = {
-//     attach: function (context, settings) {
-// 			// open video pop up
-// 			jQuery('.hero-rotator-inner .views-field-field-subhead').after('<a href="#" class="button-close">X</a><a href="#" class="button-play">Watch Video</a>');
-// 		}
-// 	};	
-// })(jQuery);
+(function ($) {
+  Drupal.behaviors.flexWorkaround = {
+    attach: function (context, settings) {
+      // open video pop up
+      jQuery('.hero-rotator-inner .views-field-field-subhead').after('<a href="#" class="button-close">X</a><a href="#" class="button-play">Watch Video</a>');
+    }
+  };	
+})(jQuery);
 
 //	Capabilities Module
 //	Rhino Hooton
