88524379ad02f1b04047022f5256b53632fcf961
Monica Medrano <monica.medrano@mirumagency.com>
2016-10-18 14:32:05 -0700

    107646 - Modifying feature video module in flex template to allow the inclusion of it to be clickable with a provided url field

--- drupal/sites/all/modules/features/flexible_template/templates/flexible-template-module--feature-video.tpl.php
+++ drupal/sites/all/modules/features/flexible_template/templates/flexible-template-module--feature-video.tpl.php
@@ -7,7 +7,13 @@
 
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="flexible-template-content">
-    <?php print render($content['title']); ?>
-    <?php print render($content); ?>
+    <?php if(isset($display_background_url) && $display_background_url == TRUE){ ?>
+        <a href="<?php print $background_url; ?>">
+        <?php print render($content['title']); ?>
+        <?php print render($content); ?></a>
+    <?php } else { ?>
+        <?php print render($content['title']); ?>
+        <?php print render($content); ?>
+    <?php } ?>
   </div>
 </div>
