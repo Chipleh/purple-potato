7e25fc4ad3bccfc55f9ff8abd62367f4e37d8676
Rhino Hooton <rhino.hooton@mirumagency.com>
2017-01-24 11:44:02 -0800

    109215 - auto select text input on click & auto clear on exit.

--- drupal/sites/all/themes/leidos/js/scripts/search-box.js
+++ drupal/sites/all/themes/leidos/js/scripts/search-box.js
@@ -15,15 +15,20 @@
                 jQuery(searchForm).toggleClass('search-active');
                 jQuery('.mobile-drop-active').removeClass('mobile-drop-active');
                 jQuery('#block-system-main-menu').removeClass('show');
+                jQuery('#search_leidos_website input[type=text]').focus();
             });
 
             // cancel click
             jQuery(cancelButton).on('click', function(e) {
                 jQuery(searchBox).toggleClass('search-active');
                 jQuery(searchForm).toggleClass('search-active');
+
+                // delay clear timer
+                setTimeout(function () {
+                    jQuery('#search_leidos_website input[type=text]').val('');
+                }, 500);
                 e.preventDefault();
             });
-
         }
     };
 })(jQuery);
