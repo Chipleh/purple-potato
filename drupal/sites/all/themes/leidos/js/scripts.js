f40508a1ee9d44e8c2a9a89e2039d4aa48296b44
Joel Raedeke <jraedeke@r2integrated.com>
2017-05-17 17:11:54 -0400

    homepage campaign mobile switch

--- drupal/sites/all/themes/leidos/js/scripts.js
+++ drupal/sites/all/themes/leidos/js/scripts.js
@@ -839,7 +839,7 @@
             var mobileback = jQuery(this).find('.hero-mobile-background').text();
             var mobilewidth = jQuery( window ).width();
             if(mobilewidth > 720){
-                jQuery(this).attr('style','background-image:' + desktopback +'");width:' + mobilewidth + ';');
+                jQuery(this).attr('style','background-image:' + desktopback +';width:' + mobilewidth + ';');
             } else {
                 jQuery(this).attr('style','background-image:url("' + mobileback +'");width:' + mobilewidth + ';');
             }
@@ -851,7 +851,7 @@
             var mobileback = jQuery(this).find('.hero-mobile-background').text();
             var mobilewidth = jQuery( window ).width();
             if(mobilewidth > 720){
-                jQuery(this).attr('style','background-image:url("' + desktopback +'");width:' + mobilewidth + ';');
+                jQuery(this).attr('style','background-image:' + desktopback +';width:' + mobilewidth + ';');
             } else {
                 jQuery(this).attr('style','background-image:url("' + mobileback +'");width:' + mobilewidth + ';');
             }
